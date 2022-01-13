04b1c3bf5077bbde6c7a459b832e671db3f67bb7
dainko <dainko3gmail.com>
2022-01-14 07:55:54 +0900

    First Commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import time
+from PIL import Image   
+
+st.title('Streamlit 超入門')
+
+st.write('Display image')
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示するよ')
+if button:
+    right_column.write('ここは右だよ')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容')
+
+st.write('プログレスバー')
+'start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration + {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+
+# option = st.text_input('あなたの趣味は？')
+
+# 'あなたの趣味は', option
+
+# condition = st.slider('あなたの調子は？',0,100,50)
+# 'コンディション', condition 
+
+# if st.checkbox('show image'):
+#     img = Image.open('image.png')
+#     st.image(img, caption='genshin', use_column_width=True)
+
+
+
+"""
+# タイトル
+
+## サブタイトル
+
+### カテゴリタイトル
+
+``` python
+import streamlit as st
+import numpy as np
+import pandas as pd
+```
+
+
+
+"""
