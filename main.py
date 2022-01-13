e8d0a77625bfe864c098750b68113d044d2a7302
dainko <dainko3gmail.com>
2022-01-14 08:09:34 +0900

    test

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from PIL import Image
 
 st.title('Streamlit 超入門')
 
-st.write('Display image')
-
-left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示するよ')
-if button:
-    right_column.write('ここは右だよ')
-
-expander = st.expander('問い合わせ')
-expander.write('問い合わせ内容')
 
 st.write('プログレスバー')
 'start!!'
@@ -24,6 +15,14 @@ for i in range(100):
     latest_iteration.text(f'Iteration + {i+1}')
     bar.progress(i+1)
     time.sleep(0.1)
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示するよ')
+if button:
+    right_column.write('ここは右だよ')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容')
 
 
 # option = st.text_input('あなたの趣味は？')
@@ -39,19 +38,19 @@ for i in range(100):
 
 
 
-"""
-# タイトル
+# """
+# # タイトル
 
-## サブタイトル
+# ## サブタイトル
 
-### カテゴリタイトル
+# ### カテゴリタイトル
 
-``` python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
+# ``` python
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# ```
 
 
 
-"""
+# """
